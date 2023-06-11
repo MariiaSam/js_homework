299a7322253bb6496907419257008e1605a43feb
MariiaSam <kontragent@ukr.net>
2023-06-11 23:08:10 +0300

    21

--- js/js_task_1/19.js
+++ js/js_task_1/19.js
@@ -9,3 +9,17 @@
 Виклик checkPassword("polyhax") повертає "Access denied, wrong password!"
 Виклик checkPassword("jqueryismyjam") повертає "Welcome!"*/
 
+function checkPassword(password) {
+    const ADMIN_PASSWORD = 'jqueryismyjam';
+    let message;
+  
+    if (password === null) { // Change this line
+      message =  'Canceled by user!';
+    } else if (password === ADMIN_PASSWORD) { // Change this line
+      message = 'Welcome!';
+    } else {
+      message = 'Access denied, wrong password!';
+    }
+  
+    return message;
+  }
