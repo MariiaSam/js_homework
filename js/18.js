b6b21a08871ce8254d43b435d9c0a64aa248b814
MariiaSam <kontragent@ukr.net>
2023-06-11 13:36:09 +0300

    18

--- js/18.js
+++ js/18.js
@@ -19,21 +19,22 @@ customerCredits - сума коштів на рахунку клієнта
 Виклик makeTransaction(500, 10, 5000) повертає "You ordered 10 droids, you have 0 credits left"*/
 
 function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
-  let message;
-  // Change code below this line
-  const totalPrice = pricePerDroid * orderedQuantity;
-  if (totalPrice < customerCredits) {
-    message = 'Insufficient funds!';
-  } else {
-    message =
-      'You ordered ${totalPrice} droids, you have ${totalPrice} credits left';
+    let message;
+    // Change code below this line
+    let totalPrice = pricePerDroid * orderedQuantity;
+    if (totalPrice > customerCredits) {
+      message = `Insufficient funds!`;
+    }
+    else {
+      message = `You ordered ${orderedQuantity} droids, you have ${customerCredits - totalPrice} credits left`
+    }
+    // Change code above this line
+    return message;
   }
-
-  // Change code above this line
-  return message;
-}
-console.log(makeTransaction(3000, 5, 23000));
-console.log(makeTransaction(1000, 3, 15000));
-console.log(makeTransaction(5000, 10, 8000));
-console.log(makeTransaction(2000, 8, 10000));
-console.log(makeTransaction(500, 10, 5000));
+    console.log(makeTransaction(3000, 5, 23000));
+    console.log(makeTransaction(1000, 3, 15000));
+    console.log(makeTransaction(5000, 10, 8000));
+    console.log(makeTransaction(2000, 8, 10000));
+    console.log(makeTransaction(500, 10, 5000));
+    
+  
