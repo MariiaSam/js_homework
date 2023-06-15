32842c4fdc19d679f7b64592b5029096e745813a
MariiaSam <kontragent@ukr.net>
2023-06-15 20:04:33 +0300

    notes + tasks

---
+++ js/js_task_1/14.js
@@ -0,0 +1,13 @@
+function isValidPassword(password) {
+    const SAVED_PASSWORD = 'jqueryismyjam';
+    // Change code below this line
+    const isMatch = password === 'jqueryismyjam';
+  
+    // Change code above this line
+    return isMatch;
+  }
+  
+  console.log(isValidPassword('mangodab3st'));
+  console.log(isValidPassword('kiwirul3z'));
+  console.log(isValidPassword('jqueryismyjam'));
+  
