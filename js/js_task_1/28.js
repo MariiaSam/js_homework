940ce9fd364726856c9395733bcbe74184a7dae5
MariiaSam <kontragent@ukr.net>
2023-06-16 13:15:58 +0300

    29

--- js/js_task_1/28.js
+++ js/js_task_1/28.js
@@ -15,29 +15,26 @@
 Виклик checkPassword("jqueryismyjam") повертає "Welcome!"
 */
 function checkPassword(password) {
-    const ADMIN_PASSWORD = 'jqueryismyjam';
-    let message;
-    // Change code below this line
-  
-    switch (password) {
-      case null:
-        message = 'Canceled by user!';
-        break;
-  
-      case ADMIN_PASSWORD:
-        message = 'Welcome!';
-        break;
-  
-      default:
-        message = 'Access denied, wrong password!';
-      
-  
-    }
-  
-    // Change code above this line
-    return message;
+  const ADMIN_PASSWORD = 'jqueryismyjam';
+  let message;
+  // Change code below this line
+
+  switch (password) {
+    case null:
+      message = 'Canceled by user!';
+      break;
+
+    case ADMIN_PASSWORD:
+      message = 'Welcome!';
+      break;
+
+    default:
+      message = 'Access denied, wrong password!';
   }
-  
+
+  // Change code above this line
+  return message;
+}
 
 console.log(checkPassword('mangohackzor'));
 console.log(checkPassword(null));
