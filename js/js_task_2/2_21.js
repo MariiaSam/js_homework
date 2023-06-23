42c6cee1f2a64a8ed467ef65d27d893454d8d08d
MariiaSam <kontragent@ukr.net>
2023-06-23 19:32:26 +0300

    20

---
+++ js/js_task_2/2_21.js
@@ -0,0 +1,7 @@
+// Напиши функцію findLongestWord(string), яка приймає довільний рядок, що складається тільки зі слів, розділених пробілом (параметр string), і повертає найдовше слово в цьому рядку.
+
+// Оголошена функція findLongestWord(string)
+// Виклик функції findLongestWord("The quick brown fox jumped over the lazy dog") повертає jumped
+// Виклик функції findLongestWord("Google do a roll") повертає Google
+// Виклик функції findLongestWord("May the force be with you") повертає force
+// Виклик функції findLongestWord() з випадковим рядком повертає правильне значення
