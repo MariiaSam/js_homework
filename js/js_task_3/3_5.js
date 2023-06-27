fa4152e165e99f41e955b99105d884f1e78dbe96
MariiaSam <kontragent@ukr.net>
2023-06-27 23:32:50 +0300

    1-12

---
+++ js/js_task_3/3_5.js
@@ -0,0 +1,43 @@
+// Другий спосіб отримати доступ до властивості об'єкта - це синтаксис об'єкт["ключ_властивості"]. Схоже на звернення до елемента масиву з відмінністю в тому, що в дужках зазначається не індекс елемента, а ім'я властивості як рядок.
+
+// Синтаксис «квадратних дужок» використовується значно рідше. Як правило у випадках, коли ім'я властивості заздалегідь невідоме або воно зберігається у змінній (як значення параметра функції, наприклад).
+
+// На місце звернення буде повернуто значення властивості з таким ім'ям.
+// Якщо в об'єкті відсутня властивість з таким ім'ям, на місце звернення повернеться undefined.
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// const bookTitle = book["title"];
+// console.log(bookTitle); // "The Last Kingdom"
+
+// const bookGenres = book["genres"];
+// console.log(bookGenres); // ["historical prose", "adventure"]
+
+// const propKey = "author";
+// const bookAuthor = book[propKey];
+// console.log(bookAuthor); // "Bernard Cornwell"
+
+
+// 
+
+
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"],
+  };
+  
+  // Change code below this line
+  const aptRating = apartment['rating'];
+  const aptDescr = apartment['descr'];
+  const aptPrice = apartment['price'];
+  const aptTags = apartment['tags'];
+  // Change code above this line
+  
---
+++ js/js_task_3/3_7.js
@@ -0,0 +1,41 @@
+// Операція додавання нової властивості після створення об'єкта нічим не відрізняється від зміни значення вже існуючої властивості. Якщо під час запису значення за ім'ям, така властивість відсутня в об'єкті, вона буде створена.
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   public: true,
+//   rating: 8.38,
+// };
+
+// book.pageCount = 836;
+// book.originalLanguage = "en";
+// book.translations = ["ua", "ru"];
+
+// console.log(book.pageCount); // 836
+// console.log(book.originalLanguage); // "en"
+// console.log(book.translations); // ["ua", "ru"]
+
+// 
+
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4.7,
+    price: 5000,
+    tags: ["premium", "promoted", "top", "trusted"],
+    owner: {
+      name: "Henry Sibola",
+      phone: "982-126-1588",
+      email: "henry.carter@aptmail.com",
+    },
+  };
+  apartment.area = 60;
+  apartment.rooms = 3;
+  apartment.location = {
+    country: "Jamaica",
+        city: "Kingston",
+  };
+  
+  // Change code below this line
+  
