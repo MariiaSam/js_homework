23c0d6e8f3eef40548db1915cd5108684a0dbde3
MariiaSam <kontragent@ukr.net>
2023-06-30 12:22:15 +0300

    13-18

---
+++ js/js_task_3/3_14.js
@@ -0,0 +1,22 @@
+// Виконай рефакторинг функції countProps(object), використовуючи метод Object.keys() і, можливо, але не обов'язково, цикл for...of.
+
+// Оголошена функція countProps(object)
+// Виклик countProps({}) повертає 0
+// Виклик countProps({ name: "Mango", age: 2 }) повертає 2
+// Виклик countProps({ mail: "poly@mail.com", isOnline: true, score: 500 }) повертає 3
+// Функція підраховує тільки власні властивості об'єкта
+// Функція використовує метод Object.keys() і, можливо, цикл for...of
+
+function countProps(object) {
+    // Change code below this line
+    let propCount = 0;
+    const keys = Object.keys(object);
+  
+    for (const key of keys) {
+        propCount += 1;
+      }
+  
+    return propCount;
+    // Change code above this line
+  }
+  
---
+++ js/js_task_3/3_16.js
@@ -0,0 +1,18 @@
+// Напиши функцію countTotalSalary(salaries), яка приймає об'єкт зарплат, де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата. Функція повинна розрахувати загальну суму зарплат співробітників і повернути її. Використовуй змінну totalSalary для зберігання загальної суми зарплати.
+
+// Оголошена функція countTotalSalary(salaries)
+// Виклик countTotalSalary({}) повертає 0
+// Виклик countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) повертає 330
+// Виклик countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) повертає 400
+// Функція враховує тільки власні властивості об'єкта
+
+function countTotalSalary(salaries) {
+    let totalSalary = 0;
+    // Change code below this line
+  for (let salary of Object.values(salaries)) {
+      totalSalary += salary;
+    }
+    // Change code above this line
+    return totalSalary;
+  }
+  
