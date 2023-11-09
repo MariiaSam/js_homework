468fd96f8263650fb6d2a7fb6a41d50833457ac0
MariiaSam <kontragent@ukr.net>
2023-11-09 22:45:07 +0200

    add 4.1

--- practice/practice.js
+++ practice/practice.js
@@ -841,3 +841,398 @@ const users = [{ name: 'Kate' }, { name: 'Alex' }, { name: 'Mark' }];
 // }
 
 // displayProductInfo(product);
+
+// Task - 3 - Операція spread
+//Напиши функцію createContact(partialContact) так, щоб вона повертала новий об'єкт контакту з доповненними властивостями id, createAt, а також list зі значеннями "default", якщо в partialContact немає такої властивості
+
+
+// function createContact(partialContact) {
+//  const profile = {
+//     id: Date.now(), 
+//     createAt: new Date(),
+//     list: 'default',
+//     ...partialContact
+//     }
+//     return profile
+// }
+
+// console.log(
+//       createContact({
+//         name: "Mango",
+//         email: "mango@mail.com",
+//         list: "friends",
+//       })
+//     ); 
+//     console.log(
+//       createContact({
+//         name: "Poly",
+//         email: "poly@hotmail.com",
+//       })
+//     );
+
+// Task - 4 - Операція rest
+// Напиши функцію transformUsername(user) так, щоб вона повертала новий об'єкт із властивістю fullName, замість firstName та lastName.
+ 
+// function transformUsername({firstName, lastName, ...props }) {
+//     return {
+//         fullName: `${firstName} ${lastName}`,
+//         ...props
+//     }
+
+// }
+
+// or
+
+
+// function transformUsername(obj) {
+//   const keys = Object.keys(obj);
+//   console.log(keys);
+//   const profile = {};
+//   let fullName = [];
+//   for (const key of keys) {
+//     if (key === "firstName" || key === "lastName") {
+//       fullName.push(obj[key]);
+//     } else {
+//       profile[key] = obj[key];
+//     }
+//   }
+
+// or
+
+// function transformUsername(obj) {
+//    const  firstName = obj.firstName
+//    const lastName = obj.lastName
+//    const fullName = `${firstName} ${lastName}`
+
+//    delete  obj.firstName
+//    delete obj.lastName
+
+//    obj.fullName = fullName
+
+//    return obj
+//   }
+
+
+// console.log(
+//   transformUsername({
+//     id: 1,
+//     firstName: "Jacob",
+//     lastName: "Mercer",
+//     email: "j.mercer@mail.com",
+//     friendCount: 40,
+//   })
+// );
+
+// console.log(
+//   transformUsername({
+//     id: 2,
+//     firstName: "Adrian",
+//     lastName: "Cross",
+//     email: "a.cross@hotmail.com",
+//     friendCount: 20,
+//   })
+// );
+
+// Task - 5 Об'єкт параметрів
+// Необхідно зрoбити рефакторинг функції calculateHousePerimeter, так щоб вона приймала об'єкт з параметрами будинку, включаючи довжини сторін будинку. Функція повинна розрахувати та повернути периметр будинку.
+
+// function calculateHousePerimeter({sideA, sideB, sideC, sideD} = {}) {
+//   const perimeter = sideA + sideB + sideC + sideD;
+//   return perimeter;
+// }
+
+
+// const perimeter = calculateHousePerimeter ({sideA: 10, sideB: 15, sideC: 10, sideD: 15}) 
+// об єкт параметрів  
+
+
+
+//=========================== AROW FUNCTION, FOR EACH 4.1=================
+
+// JSDoc - comment коротка відомість про функцію яка допомагає іншому розробнику швидше зрозуміти що робить, приймає та повертає функція.
+// Для створення JSDoc - comment на рядку перед створеною функціює натискаємо /**
+// Далі описуємо нашу функцію.
+
+// function declaration
+
+/**
+ * Buy product, log message with purchase info - опис функції
+ * @param {String} productName
+ * @param {Number} productQty
+ * @param {Number} price
+ */
+
+// function buyProduct(productName, productQty, price) {
+//   console.log(
+//     `You buy ${productQty} ${productName}'s, cost per item ${price}, total cost ${
+//       productQty * price
+//     }`
+//   );
+// }
+// Плюс function declaration, те що вона має поведінку хойтингу, доступна нм ще перед викликом і ініціалізацією 
+
+// buyProduct("apple", 10, 35);
+
+
+//function expression
+
+// const buyProduct = function (productName, productQty, price){
+//   console.log(
+//     `You buy ${productQty} ${productName}'s, cost per item ${price}, total cost ${
+//       productQty * price
+//     }`
+//   );
+// }
+// buyProduct("apple", 10, 35);
+
+// обмеження, ф-ція є анонімною і для її виклику явно задаєм їй ім я, ми не можемо її викликати перед її ініціалізацією 
+
+// ================================arrow function================
+
+// const buyProduct = (productName, productQty, price) => {
+//   console.log(
+//     `You buy ${productQty} ${productName}'s, cost per item ${price}, total cost ${
+//       productQty * price
+//     }`
+//   );
+// };
+// buyProduct("apple", 10, 35);
+
+
+// ***********Неявне повернення******** \\
+
+// Явне повернення
+/**
+ * Get product total price
+ * @param {Number} qty - quantity items
+ * @param {Number} price - price per item
+ * @returns {Number} - total price
+ */
+// const getTotalPrice = (qty, price) => {
+//     return qty * price;
+
+// };
+// getTotalPrice(3, 150);
+
+// Один параметр з дужками
+/**
+ * Check user strong password 😂
+ * @param {String} password
+ * @returns {Boolean}
+ */
+// const checkPassword = (password) => password === "qwerty111";
+// const checkPassword = password => password === "qwerty111";
+
+// console.log(checkPassword('qwerty111'));
+// console.log(checkPassword('qwerty222'));
+
+// **************************************Callback function*********************************************\\
+
+// *******Callback функція у вигляді посилання на функції*****\\
+
+// function logResult (a, b, callback){
+// console.log(callback)
+// const result = callback (a, b)
+// console.log(`Result is ${result}`)
+
+
+// }
+
+// logResult(4, 5, add)
+// logResult(4, 5, sum)
+
+// function add(a, b) {
+//     return a + b;
+//   }
+  
+//   function sum(a, b) {
+//       return a * b;
+//   }
+
+
+
+// ******************Інлайнова Callback функція******************\\
+// function logResult(a, b, callback) {
+//     // console.log(callback);
+//     const result = callback(a, b);
+//     console.log(`Result is ${result}`);
+// }
+
+// logResult(4, 5, (a, b) => a + b)
+// logResult(4, 5, (a, b) => a * b)
+// logResult(4, 5, (a, b) => a - b)
+// logResult(4, 5, (a, b) => a / b)
+// logResult(4, 5, (a, b) => a ** b)
+// logResult(4, 5, (a, b) => a % b)
+
+// ************************************** forEach *********************************************\\
+
+// const products = ["Apple", "Banana", "Orange", "Mango", "Watermelon"];
+
+// Цикл for
+
+// for (let i = 0; i < products.length; i+= 1) {
+//     const product = products[i];
+
+//     console.log(`${i + 1} - ${product}`)
+    
+// }
+
+// // forEach - нічого не повертає
+
+// products.forEach((product, idx, arr) => console.log(`${idx + 1} - ${product}`))
+// products.forEach(function (product, idx, arr) { console.log(`${idx + 1} - ${product}`))}
+
+// function logItem(product, idx) {
+//     console.log(`${idx + 1} - ${product }`)
+// }
+
+// products.forEach(logItem)
+
+// function customForEach (arr, callback) {
+//     for (let i = 0; i<arr.length; i+= 1) {
+//     callback(arr[i], i, arr)
+//     }
+// }
+
+// customForEach(products, (product, idx) => console.log(`${idx +1 } - ${product}`));
+
+
+// ******************************************* Практика **************************************************\\
+
+// Task - 1
+// Напишіть наступні функції:
+// createProduct(obj, callback) - приймає об'єкт товару без id, а також коллбек. Функція створює об'єкт товару, додаючи йому унікальний ідентифікатор у властивість id та викликає коллбек передаючи йому створений об'єкт.
+// logProduct(product) - колббек що приймає об'єкт продукту і логуючий його в консоль
+// logTotalPrice(product) - колббек, що приймає об'єкт продукту і логіює загальну вартість товару в консоль
+
+// function createProduct(obj, callback) {
+//     const product ={
+//     id: Date.now(), 
+//     ...obj
+//     }
+// callback(product); 
+// }
+
+// function logProduct(product) {
+//   console.log(product);
+// }
+
+// function logTotalPrice({ price, quantity }) {
+//     console.log(`Product total price ${price * quantity}`);
+// }
+
+// createProduct({ name: "🍎", price: 30, quantity: 3 }, logProduct);
+// createProduct({ name: '🍋', price: 20, quantity: 5 }, logTotalPrice);
+
+
+
+
+// Task - 2
+// Напишіть функцію яка буде отримувати 2 параметри.
+// 1 - масив чисел.
+// 2 - функцію яка має опрацювати кожен елемент масиву.
+//
+// Функція повертає новий масив кожен елемент якого є результатом виконання колбек функції
+// callback
+
+ // action - callback
+// function each(arr, action) {
+// const items = [];
+
+// // arr.forEach(element => {
+// //     const result = action(element)
+// //     items.push(result)
+// // });
+
+// // OR
+
+// arr.forEach(item =>  items.push(action(item)));
+
+// return items
+// }
+
+
+// console.log(each([64, 49, 36, 25, 16], (value) => value * 2));
+// console.log(each([64, 49, 36, 25, 16], value => value - 10));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.ceil(value)));
+// console.log(each([1.5, 2.1, 16.4, 9.7, 11.3], value => Math.floor(value)));
+
+
+// Task-3
+// Напишіть функцію getProductDetails, яка приймає ідентифікатор товару productId та дві колбек-функції successCallback та errorCallback.
+// Функція getProductDetails повинна отримати деталі про вказаний товар та передати їх у successCallback.
+// У випадку якщо товар не знайдено, викликається errorCallback і передається повідомлення про помилку.
+
+// const products = [
+//   {
+//     id: 1,
+//     name: "Телефон",
+//     price: 10000,
+//     description: "Смартфон з високоякісним дисплеєм та потужним процесором.",
+//   },
+//   {
+//     id: 2,
+//     name: "Ноутбук",
+//     price: 25000,
+//     description: "Легкий та потужний ноутбук для роботи та розваг.",
+//   },
+//   {
+//     id: 3,
+//     name: "Планшет",
+//     price: 8000,
+//     description: "Компактний планшет для перегляду контенту.",
+//   },
+// ];
+
+// function getProductDetails(id, success, error) {
+//      for (const product of products) {
+//         if(product.id === id) {
+//             success(product)
+//             reeturn;
+//         }
+//     }
+//     error(id)
+// }
+
+// getProductDetails(3, handleSuccess, handleError);
+// getProductDetails(44, handleSuccess, handleError)
+
+// function handleSuccess(message) {
+//   console.log(`✅ Success!`, message);
+// }
+
+// function handleError(productId) {
+//   console.log(`❌ Error! Product with ${productId} id not found`);
+// }
+
+// Task-4
+// Напишіть функцію calculateAveragePrice, яка приймає на вхід масив об'єктів, що представляють товари, та колбеки onSuccess та onError.
+// Функція повинна обчислювати середню ціну товарів у масиві та викликати колбек onSuccess, передаючи йому отриману середню ціну як аргумент.
+// Якщо масив товарів порожній, функція повинна викликати колбек onError, передаючи йому рядок "No products found".
+// Для форматування середньої ціни використовується метод toFixed(2), щоб обмежити число до двох десяткових знаків.
+
+const products = [
+  { name: "Apple", price: 0.99 },
+  { name: "Banana", price: 0.49 },
+  { name: "Orange", price: 0.79 },
+  { name: "Mango", price: 1.99 },
+];
+
+function calculateAveragePrice(products, onSuccess, onError) {
+
+
+}
+
+
+const onSuccess = (averagePrice) =>{}
+
+const onError = (errorMessage) => {};
+
+calculateAveragePrice(products, onSuccess, onError);
+
+
+// console.log(singl.getElem(10));
+// console.log(singl.getElem(20));
+// console.log(singl.getElem(30));
+// console.log(singl.getElem(40))
